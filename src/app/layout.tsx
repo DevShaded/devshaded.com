0cc0602182fa10b1737ecb6f32bd7550b1c8ce06
DevShaded <devshaded@devshaded.com>
2023-10-03 21:40:52 +0200

    feat: add base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout/Layout'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -10,13 +10,19 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+    <body className="flex h-full bg-zinc-50 dark:bg-black">
+    <Providers>
+      <div className="flex w-full">
+        <Layout>{children}</Layout>
+      </div>
+    </Providers>
+    </body>
     </html>
   )
 }
