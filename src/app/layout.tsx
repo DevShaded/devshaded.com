26f25fcf5ef7cab9ff9f39cdb987195e9aa22de2
DevShaded <devshaded@devshaded.com>
2023-11-21 08:44:46 +0100

    feat: upgrade to next 14 & Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,61 +5,70 @@ import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout/Layout'
 import { Poppins } from "next/font/google";
 import { clsx } from 'clsx'
-import { Analytics } from '@vercel/analytics/react';
+import Script from "next/script";
 
 const poppins = Poppins({
-    weight: ['400', '500', '700'],
-    style: ['normal', 'italic'],
-    subsets: ['latin'],
-    display: 'swap',
+  weight: ['400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
 })
 
 export const metadata: Metadata = {
-    title: {
-        template: '%s - DevShaded',
-        default:
-            'DevShaded - Web developer, photographer, and entrepreneur.',
-    },
-    description:
-        'The Fullstack Web Developer from Norway, here is the official portfolio for DevShaded',
-    twitter: {
-        card: 'summary',
-        title: 'DevShaded - Web developer, photographer, and entrepreneur.',
-        description: 'The Fullstack Web Developer from Norway, here is the official portfolio for DevShaded',
-        images: '/favicons/devshaded/android-chrome-512x512.png',
-    },
-    openGraph: {
-        type: 'website',
-        locale: 'no_NO',
-        siteName: 'DevShaded - Web developer, photographer, and entrepreneur.',
-        title: 'DevShaded - Web developer, photographer, and entrepreneur.',
-        description: 'The Fullstack Web Developer from Norway, here is the official portfolio for DevShaded',
-        images: [
-            {
-                url: '/favicons/devshaded/android-chrome-512x512.png',
-                width: 180,
-                height: 180,
-                alt: 'DevShaded - Web developer, photographer, and entrepreneur.',
-            },
-        ],
-    }
+  title: {
+    template: '%s - DevShaded',
+    default:
+      'DevShaded - Web developer, photographer, and entrepreneur.',
+  },
+  description:
+    'The Fullstack Web Developer from Norway, here is the official portfolio for DevShaded',
+  twitter: {
+    card: 'summary',
+    title: 'DevShaded - Web developer, photographer, and entrepreneur.',
+    description: 'The Fullstack Web Developer from Norway, here is the official portfolio for DevShaded',
+    images: '/favicons/devshaded/android-chrome-512x512.png',
+  },
+  openGraph: {
+    type: 'website',
+    locale: 'no_NO',
+    siteName: 'DevShaded - Web developer, photographer, and entrepreneur.',
+    title: 'DevShaded - Web developer, photographer, and entrepreneur.',
+    description: 'The Fullstack Web Developer from Norway, here is the official portfolio for DevShaded',
+    images: [
+      {
+        url: '/favicons/devshaded/android-chrome-512x512.png',
+        width: 180,
+        height: 180,
+        alt: 'DevShaded - Web developer, photographer, and entrepreneur.',
+      },
+    ],
+  }
 }
 
 export default function RootLayout({
-                                       children,
+                                     children,
                                    }: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
-    return (
-        <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-        <body className={clsx(poppins.className, 'flex h-full bg-zinc-50 dark:bg-black')}>
-        <Providers>
-            <div className="flex w-full">
-                <Layout>{children}</Layout>
-            </div>
-        </Providers>
-        <Analytics/>
-        </body>
-        </html>
-    )
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+    <body className={clsx(poppins.className, 'flex h-full bg-zinc-50 dark:bg-black')}>
+    <Providers>
+      <div className="flex w-full">
+        <Layout>{children}</Layout>
+      </div>
+    </Providers>
+    <Script src="https://www.googletagmanager.com/gtag/js?id=320162364"/>
+    <Script id="google-analytics">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '320162364');
+        `}
+    </Script>
+    </body>
+    </html>
+  )
 }
